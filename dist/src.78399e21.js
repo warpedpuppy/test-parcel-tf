854bd250f095e28dc4cd13dcd04b2dc49299ae71
ted <ted@warpedpuppy.com>
2021-05-03 10:50:41 -0400

    build

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -29949,7 +29949,7 @@ var horrorTimeApp = /*#__PURE__*/function (_React$Component) {
 
 var container = document.getElementsByClassName('app-container')[0]; // Tells React to render your app in the root DOM element
 
-_reactDom.default.render( /*#__PURE__*/_react.default.createElement(horrorTimeApp), container);
+_reactDom.default.render(_react.default.createElement(horrorTimeApp), container);
 },{"react":"../node_modules/react/index.js","react-dom":"../node_modules/react-dom/index.js","./components/main-view/main-view":"components/main-view/main-view.jsx","./index.scss":"index.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -29978,7 +29978,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52975" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53365" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
